d1ceffdbbb769bca61a964f61c64614c4aaedd6b
Vanessa Spear <vanessavspear@gmail.com>
2023-01-12 13:59:34 -0600

    added code comments

--- request_handler.py
+++ request_handler.py
@@ -3,21 +3,12 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from views import get_all_locations, get_single_location, create_location, delete_location, update_location
 from views import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
-from views import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
+from views import get_all_customers, get_single_customer, create_customer, update_customer
 from views import verify_data
 
-
-# Here's a class. It inherits from another class.
-# For now, think of a class as a container for functions that
-# work together for a common purpose. In this case, that
-# common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
-    # This is a Docstring it should be at the beginning of all classes and functions
-    # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
-
-    # Here's a class function
 
     def parse_url(self, path):
         """Splits URL to determine if a client requested an entire resource or a single dictionary
@@ -28,32 +19,24 @@ class HandleRequests(BaseHTTPRequestHandler):
         Returns:
             tuple: (resource, id)
         """
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
+
         path_params = path.split("/")
         resource = path_params[1]
         id = None
 
-        # Try to get the item at index 2
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
             id = int(path_params[2])
         except IndexError:
-            pass  # No route parameter exists: /animals
+            pass
         except ValueError:
-            pass  # Request had trailing slash: /animals/
+            pass
 
-        return (resource, id)  # This is a tuple
+        return (resource, id)
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any GET request.
     def do_GET(self):
         """Handles GET requests to the server
         """
-        response = {}  # Default response
+        response = {} 
 
         # Parse the URL and capture the tuple that is returned
         (resource, id) = self.parse_url(self.path)
@@ -96,8 +79,6 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         self.wfile.write(json.dumps(response).encode())
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any POST request.
     def do_POST(self):
         """Handles POST requests to the server"""
 
@@ -111,9 +92,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Add a new animal to the list. Don't worry about
-        # the orange squiggle, you'll define the create_animal
-        # function next.
         if resource == "animals":
             # Verify post properties match resource properties
             verified_data = verify_data(resource, post_body)
@@ -181,7 +159,6 @@ class HandleRequests(BaseHTTPRequestHandler):
                 # Encode the new customer and send in response
                 self.wfile.write(json.dumps(verified_data).encode())
 
-    # A method that handles any PUT request.
     def do_PUT(self):
         """Handles PUT requests to the server
         """
@@ -213,7 +190,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.wfile.write("".encode())
 
     def _set_headers(self, status):
-        # Notice this Docstring also includes information about the arguments passed to the function
         """Sets the status code, Content-Type and Access-Control-Allow-Origin
         headers on the response
 
@@ -225,7 +201,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
-    # Another method! This supports requests with the OPTIONS verb.
     def do_OPTIONS(self):
         """Sets the options headers
         """
@@ -240,33 +215,35 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_DELETE(self):
         """Deletes dictionary from database
         """
-        # Set a 204 response code
-        self._set_headers(204)
 
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Delete a single animal from the list
-        if resource == "animals":
-            delete_animal(id)
-
-        # Delete a single location from the list
-        if resource == "locations":
-            delete_location(id)
-
-        # Delete a single location from the list
-        if resource == "employees":
-            delete_employee(id)
-
-        # Delete a single customer from the list
+        # Prevent client from deleting customer data
         if resource == "customers":
-            delete_customer(id)
+            self._set_headers(405)
 
-        # Encode the new item and send in response
-        self.wfile.write("".encode())
+            # Encode the new item and send in response
+            self.wfile.write("".encode())
+        else:
+            # Set a 204 response code
+            self._set_headers(204)
 
-# This function is not inside the class. It is the starting
-# point of this application.
+            # Delete a single animal from the list
+            if resource == "animals":
+                delete_animal(id)
+
+            # Delete a single location from the list
+            if resource == "locations":
+                delete_location(id)
+
+            # Delete a single location from the list
+            if resource == "employees":
+                delete_employee(id)
+
+            # Encode the new item and send in response
+            self.wfile.write("".encode())
+
 def main():
     """Starts the server on port 8088 using the HandleRequests class
     """
